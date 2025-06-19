a18380c3d36984b87c36cf684a427568d23d0678
Sergio López <sergiolopezlp@gmail.com>
2025-06-19 21:27:57 +0100

    Deployed 42c9cc75a to 0.2 with MkDocs 1.6.0 and mike 2.1.3

---
+++ 0.2/js/custom.js
@@ -0,0 +1,65 @@
+const processLinksAndImages = () => {
+    const currentBase = window.location.origin;
+    const currentPath = window.location.pathname;
+    const versionRegex = /^\/([^\/]+)\//;
+    const match = currentPath.match(versionRegex);
+
+    if (!match) {
+    return;
+    }
+
+    const currentVersion = match[1];
+
+    const links = document.querySelectorAll("a[href], img[src]");
+
+    const promises = [];
+
+    links.forEach((link) => {
+    const attr = link.tagName === "IMG" ? "src" : "href";
+    const url = link.getAttribute(attr);
+
+    if (/^(https?:|mailto:|#|\/\/.+)/.test(url)) {
+        if (!url.startsWith(currentBase)) return;
+        if (url.startsWith(`${currentBase}/${currentVersion}/`)) return;
+    } else {
+        if (url.startsWith(`/${currentVersion}/`)) return;
+    }
+
+    let newUrl;
+    if (url.startsWith("/")) {
+        newUrl = `/${currentVersion}${url}`;
+    } else {
+        newUrl = `${currentBase}/${currentVersion}${url.replace(currentBase, "")}`;
+    }
+
+    promises.push(
+        new Promise(() => {
+        fetch(newUrl);
+        link.setAttribute(attr, newUrl)
+        })
+    )
+    });
+
+    Promise.all(promises)
+};
+
+if (document.readyState === "loading") {
+    document.addEventListener("readystatechange", () => {
+    if (document.readyState === "interactive") {
+        processLinksAndImages();
+    }
+    });
+} else {
+    processLinksAndImages();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const basePath = window.location.pathname;
+
+    const latestVersionPath = '/latest/';
+    const currentHash = window.location.hash; // Captura cualquier hash presente (#pokemon-interactions, etc.)
+
+    if (!basePath.startsWith(latestVersionPath)) {
+      window.location.replace(latestVersionPath + currentHash);
+    }
+});
